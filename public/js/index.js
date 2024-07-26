7053b57d6b69981e040e0ecd223a5a273fa83f9d
= <iresonancei@mail.ru>
2024-07-26 14:08:39 +0300

    initialize

---
+++ public/js/index.js
@@ -0,0 +1,34 @@
+new Swiper(".swiper", {
+  loop: true,
+  grabCursor: true,
+  autoplay: {
+    delay: 3e3,
+  },
+  effect: "cards",
+});
+
+const gallery = document.getElementById("gallery");
+
+let zoomCount = 0;
+const maxZoomCount = 2;
+
+gallery.addEventListener("click", (e) => {
+  if (zoomCount !== maxZoomCount) e.preventDefault();
+
+  switch (zoomCount) {
+    case 1: {
+      gallery.classList.remove("gallery__grid--2");
+      gallery.classList.add("gallery__grid--1");
+      zoomCount++;
+      return;
+    }
+    case 2:
+      return;
+    default: {
+      gallery.classList.remove("gallery__grid--3");
+      gallery.classList.add("gallery__grid--2");
+      zoomCount++;
+      return;
+    }
+  }
+});
