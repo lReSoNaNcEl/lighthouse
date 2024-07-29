b5b86ccd96e9124471ef4acf97f78c916bae3170
= <iresonancei@mail.ru>
2024-07-29 21:33:26 +0300

    integration particles.js lib

--- public/js/index.js
+++ public/js/index.js
@@ -1,34 +1,79 @@
-new Swiper(".swiper", {
-  loop: true,
-  grabCursor: true,
-  autoplay: {
-    delay: 3e3,
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: "#ffffff",
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: "img/github.svg",
+        width: 100,
+        height: 100,
+      },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: false,
+    },
+    move: {
+      enable: true,
+      speed: 3,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
   },
-  effect: "cards",
-});
-
-const gallery = document.getElementById("gallery");
-
-let zoomCount = 0;
-const maxZoomCount = 2;
-
-gallery.addEventListener("click", (e) => {
-  if (zoomCount !== maxZoomCount) e.preventDefault();
-
-  switch (zoomCount) {
-    case 1: {
-      gallery.classList.remove("gallery__grid--2");
-      gallery.classList.add("gallery__grid--1");
-      zoomCount++;
-      return;
-    }
-    case 2:
-      return;
-    default: {
-      gallery.classList.remove("gallery__grid--3");
-      gallery.classList.add("gallery__grid--2");
-      zoomCount++;
-      return;
-    }
-  }
+  interactivity: {
+    detect_on: "window",
+    events: {
+      onhover: {
+        enable: false,
+      },
+      onclick: {
+        enable: false,
+      },
+      resize: true,
+    },
+  },
+  retina_detect: true,
 });
---
+++ public/js/swiper.js
@@ -0,0 +1,8 @@
+new Swiper(".swiper", {
+  loop: true,
+  grabCursor: true,
+  autoplay: {
+    delay: 3e3,
+  },
+  effect: "cards",
+});
