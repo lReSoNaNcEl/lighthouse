e6caea1b9c483ff7f03798792a37a172800fb325
= <iresonancei@mail.ru>
2024-07-29 21:52:48 +0300

    settings vite build

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,18 @@ import { defineConfig } from 'vite'
 import path from 'path';
 
 export default defineConfig({
-    root: path.resolve('./public'),
+    root: path.resolve('./src'),
     server: {
         port: 8080,
+    },
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: path.resolve(__dirname, './src/index.html'),
+                new_directions: path.resolve(__dirname, './src/new-directions.html')
+            }
+        }
     }
 })
