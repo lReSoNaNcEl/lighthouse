201dd74cbd40b267714def404a20eef1a5406891
= <iresonancei@mail.ru>
2024-09-23 11:39:26 +0300

    updated public dir

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
     server: {
         port: 8080,
     },
-    publicDir: '',
+    publicDir: 'public',
     build: {
         manifest: true,
         outDir: '../dist',
